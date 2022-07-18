dcc674d05bf1c5a98934ef51ffb1cd39bdd1c640
izaelbm <46963796+izaelbm@users.noreply.github.com>
2022-07-18 19:19:16 -0300

    Create whois.sh

---
+++ whois.sh
@@ -0,0 +1,23 @@
+ #!/bin/bash
+
+echo "" > whois_log.txt
+
+mode="Modo de Uso: ./whois.sh list.txt"
+
+#Definindo Variaveis
+lista=$1
+
+for i in $(cat $lista)
+do
+
+result=$(whois $i | grep "Organization:" | cut -d ":" -f 2 | sed 's/ //g')
+
+if [ -z = $result ]; then 
+        echo $i";Other"
+else
+        echo $i";"$result
+fi
+
+sleep 1
+
+done
